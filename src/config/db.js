0d4a7d254b271809ea12f961f2ec4f14deb2ac91
FabsSWD <fabarcamonge@gmail.com>
2025-04-11 13:56:28 -0600

    feat: implement database initialization and create events table

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+sqlite3.verbose()
+
+export async function initializeDb () {
+  const db = await open({
+    filename: './database.sqlite',
+    driver: sqlite3.Database
+  })
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS events (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      date TEXT NOT NULL
+    );
+  `)
+
+  return db
+}
