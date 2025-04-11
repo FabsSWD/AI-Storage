ef7db7fe19abb48c6c2a8a5db38f12b969101f09
FabsSWD <fabarcamonge@gmail.com>
2025-04-11 14:10:56 -0600

    feat: set up event routes and handle 404 errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import eventsRouter from './routes/event.route.js'
 
 dotenv.config()
 
@@ -8,10 +9,12 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use('/events', eventsRouter)
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not found.' })
 })
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:' + PORT)
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
