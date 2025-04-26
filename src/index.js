e15098ff3206dc067d02ca2c2089bcd4ccd0937e
Jozandro23 <lizandroht2002@gmail.com>
2025-04-25 19:02:13 -0600

    Refactor: updated accesible ports

--- src/index.js
+++ src/index.js
@@ -54,6 +54,6 @@ app.use((req, res, next) => {
 })
 
 // Inicia el servidor y escucha en el puerto especificado
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
